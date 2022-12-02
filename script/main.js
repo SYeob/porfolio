3faa8ab8acb8b1f095d3de03a530b90e5ddef6b0
soonyeob <116706311+SYeob@users.noreply.github.com>
2022-12-02 12:18:03 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
   // fullpage toggle
-  $('#fullpage').fullpage({
+  $("#fullpage").fullpage({
     autoScrolling: true,
     scrollHorizontally: true,
     navigation: true,
   });
+    $('.sec1-content-left').fadeIn(1000);
+
+  setTimeout(function(){
+    $("#fourGrit").fadeIn();
+    $("#fourGrit").css('color', '#2ad2c9')
+
+  },1000)
 });
